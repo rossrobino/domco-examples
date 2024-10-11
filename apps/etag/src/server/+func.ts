542bc0111cfef551c81672b2205712bb7682ecb2
rossrobino <rbrobino@gmail.com>
2024-10-11 13:08:45 -0400

    json etag example

--- apps/etag/src/server/+func.ts
+++ apps/etag/src/server/+func.ts
@@ -1,19 +1,36 @@
 import { html } from "client:page";
 import type { Handler } from "domco";
 
-export const prerender = ["/"];
-
 export const handler: Handler = (req) => {
-	const eTag = `"${djb2(html)}"`;
-	const notModified = eTag === req.headers.get("If-None-Match");
+	const { pathname } = new URL(req.url);
 
-	return new Response(notModified ? null : html, {
-		status: notModified ? 304 : 200,
-		headers: {
-			"Content-Type": "text/html",
-			ETag: eTag,
-		},
-	});
+	if (pathname === "/") {
+		const eTag = `"${djb2(html)}"`;
+		const notModified = eTag === req.headers.get("If-None-Match");
+
+		return new Response(notModified ? null : html, {
+			status: notModified ? 304 : 200,
+			headers: {
+				"Content-Type": "text/html",
+				ETag: eTag,
+			},
+		});
+	} else if (pathname === "/json") {
+		const jsonString = JSON.stringify({ hello: "world" });
+
+		const eTag = `"${djb2(jsonString)}"`;
+		const notModified = eTag === req.headers.get("If-None-Match");
+
+		return new Response(notModified ? null : jsonString, {
+			status: notModified ? 304 : 200,
+			headers: {
+				"Content-Type": "application/json",
+				ETag: eTag,
+			},
+		});
+	}
+
+	return new Response("Not found.", { status: 404 });
 };
 
 const djb2 = (...values: (string | ArrayBufferView)[]) => {
